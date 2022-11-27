e42780c0334ff7f8af36231af54d79545ac2e63b
Alastair <alastair.chau@gmail.com>
2022-11-27 19:35:35 +0000

    view test passing

--- tests/newsView.test.js
+++ tests/newsView.test.js
@@ -1,10 +1,12 @@
 /**
  * @jest-environment jsdom
  */
+// need to 'npm install jest-environment-jsdom'
 
 const fs = require('fs');
 const NewsView = require('../src/newsView');
 const NewsModel = require('../src/newsModel');
+const NewsClient = require('../src/newsClient');
 /*
 Makes fetch available to our test (it is not by default). Normally fetch is only available within the browser.
 Will need to run: npm install --save- jest-fetch-mock
@@ -15,9 +17,10 @@ require('jest-fetch-mock').enableMocks();
 describe('NotesView', () => {
   
   beforeEach(() => {
-    document.body.innerHTML = fs.readFileSync('../index.html'); // sets the jest 'document' HTML to a mock html webpage for the tests
+    document.body.innerHTML = fs.readFileSync('./index.html'); // sets the jest 'document' HTML to a mock html webpage for the tests
+    client = new NewsClient();
     model = new NewsModel();
-    view = new NewsView(model, clientMock);
+    view = new NewsView(model, client);
   });
   
   it('fetches the mock a passes through the news data', (done) => {
