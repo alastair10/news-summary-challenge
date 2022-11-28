43f3664396a5a6d1b855238f224a0291ff829803
Alastair <alastair.chau@gmail.com>
2022-11-28 09:56:51 +0000

    updated CSS styling

--- src/index.js
+++ src/index.js
@@ -6,19 +6,3 @@ const apiKey = require('../apiKey');
 const client = new NewsClient();
 const model = new NewsModel();
 const view = new NewsView(model, client);
-
-
-
-// const { get } = require('callback-fetch');
-
-// const apiKey = require('../apiKey');
-// const apiURL = `https://content.guardianapis.com/search?api-key=${apiKey}`;
-
-// let newsData = null;
-
-// get(apiURL).then((response) => {
-//   newsData = JSON.parse(response.body);
-//   console.log(newsData);
-// });
-
-// console.log('Requesting news data');
